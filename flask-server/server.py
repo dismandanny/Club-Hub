75975c9b89a8324d4a60ffa8f411cb3a0fbd91df
Danny Disman <dismandanny@LAPTOP-IUAI9C4V.localdomain>
2023-09-09 14:23:36 -0400

    final touches

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 # Members API Route
-@app.route("/members")
+@app.route("/")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    # return {"members": ["Member1", "Member2", "Member3"]}
+    return render_template("basic.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
